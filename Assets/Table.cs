1a4eb0b8df1739d8a59bb66956b29ad692581e0b
Wo1fbite <kwulf40@gmail.com>
2020-11-15 23:56:21 -0800

    Ver 0.6551
    Small fix

--- Assets/Table.cs
+++ Assets/Table.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Table : MonoBehaviour
 {
-    public UnityEngine.Events.UnityEvent getFood;
     // Start is called before the first frame update
     void Start()
     {

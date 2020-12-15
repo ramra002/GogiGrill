d7667b5acc0db7a0fd6b89bce9393f88e10c0488
Wo1fbite <kwulf40@gmail.com>
2020-12-14 18:36:10 -0800

    InDev

--- Assets/Item.cs
+++ Assets/Item.cs
@@ -7,8 +7,28 @@ using UnityEngine.Animations;
 public class Item : MonoBehaviour
 {
     private Rigidbody rb;
+    
     public Rigidbody Rb => rb;
+
+    public Transform target;
+
+    public float range = 1;
     void Awake(){
         rb = GetComponent<Rigidbody>();
+    }
+
+    void Update(){
+        Collider[] objects = Physics.OverlapSphere(transform.position, range);
+
+        wallCheck(objects);
+    }
+
+    void wallCheck(Collider[] objects){
+        foreach(Collider newObject in objects){
+            if (newObject.tag == "Wall"){
+                target = GameObject.Find("Player").transform;
+                transform.position = (transform.position - target.position);
+            }
+        }
     }
 }
